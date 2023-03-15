6b5f0d4c51723bd23d0f186a282a8b40e3695ad0
BhargavaReddy143 <bannybhe@gmail.com>
2023-03-15 16:40:59 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Dhoni(request):
+    return HttpResponse('He Is The Best Finisher Now And Ever')
+def Raina(request):
+    return HttpResponse('He Is Known As Mr IPL')    
---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Jaddu(request):
+    return HttpResponse('He Is The Current Test No1 AllRounder')
+def bapu(request):
+    return HttpResponse('Another Name Of Jadeja')    
